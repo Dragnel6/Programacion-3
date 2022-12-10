2d728a159e496d78ca948d04aabb63b4013921c2
valoz <cj5895576@gmail.com>
2022-12-10 18:18:24 +0100

    IA actualizado

--- SABINA.py
+++ SABINA.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 
-name = "sabina"
+name = "sabina "
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
@@ -10,6 +10,7 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[3].id)
 
 def talk(text):
+    print(text)
     engine.say(text)
     engine.runAndWait()
 
@@ -20,21 +21,19 @@ def listen():
             compu = listener.listen(source)
             recs = listener.recognize_google(compu)
             recs = recs.lower()
-            if name in recs:
-                recs = recs.replace(name, '')
-
-
+            print('Has mencionado: ' + recs)
     except:
         pass
     return recs    
    
 def run_sabina():
     rec  = listen()
-    if 'reproduce' in rec:
-        musica = rec.replace('reproduce', '')
-        print("Reproduciendo ", + musica)
-        talk("Reproduciendo ", + musica)
-        pywhatkit.playonyt(musica)
+    if name in rec:
+        rec = rec.replace(name, '')
+        if 'reproduce' in rec:
+            musica = rec.replace('reproduce ', '')
+            talk("Reproduciendo " + musica)
+            pywhatkit.playonyt(musica)
 
 if __name__ == '__main__':
     run_sabina()  
